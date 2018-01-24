fb2d2ff4b56a540d7100ac987e7ace5dac8253ac
Joachim Løvgaard <joachim@loevgaard.dk>
2018-01-24 11:01:09 +0100

    added product type repos

---
+++ src/Repository/ProductTypeRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Loevgaard\DandomainFoundation\Repository;
+
+use Loevgaard\DandomainFoundation\Entity\Generated\ProductTypeInterface;
+use Loevgaard\DandomainFoundation\Entity\ProductType;
+use Loevgaard\DandomainFoundation\Repository\Generated\ProductTypeRepositoryTrait;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method null|ProductTypeInterface find($id)
+ * @method ProductTypeInterface[] findBy(array $criteria, array $orderBy = null, int $limit = null, int $offset = null)
+ * @method null|ProductTypeInterface findOneBy(array $criteria)
+ * @method ProductTypeInterface[] findAll()
+ */
+class ProductTypeRepository extends AbstractRepository
+{
+    use ProductTypeRepositoryTrait;
+
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, ProductType::class);
+    }
+}

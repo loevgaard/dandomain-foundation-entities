628a152429ddbcbf0343e3a965a5ccae3bb2a29d
Joachim Løvgaard <joachim@loevgaard.dk>
2018-01-24 10:48:04 +0100

    added tag value repos

---
+++ src/Repository/TagValueRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Loevgaard\DandomainFoundation\Repository;
+
+use Loevgaard\DandomainFoundation\Entity\Generated\TagValueInterface;
+use Loevgaard\DandomainFoundation\Entity\TagValue;
+use Loevgaard\DandomainFoundation\Repository\Generated\TagRepositoryTrait;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method null|TagValueInterface find($id)
+ * @method TagValueInterface[] findBy(array $criteria, array $orderBy = null, int $limit = null, int $offset = null)
+ * @method null|TagValueInterface findOneBy(array $criteria)
+ * @method TagValueInterface[] findAll()
+ */
+class TagValueRepository extends AbstractRepository
+{
+    use TagRepositoryTrait;
+
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, TagValue::class);
+    }
+
+    public function findOneByExternalId(int $externalId): ?TagValueInterface
+    {
+        /** @var TagValueInterface $obj */
+        $obj = $this->_findOneByExternalId($externalId);
+
+        return $obj;
+    }
+}
